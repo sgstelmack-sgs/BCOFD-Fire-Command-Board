a0aa197bfd6917293cffbd1f727691794a118b13
Stephen Stelmack <s.g.stelmack@gmail.com>
2026-02-09 22:12:35 +0000

    Configure deployment paths

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/BCOFD-Fire-Command-Board/", // This is the repo name for GitHub Pages
   server: {
     allowedHosts: ["qwyzs9-5173.csb.app"],
   },
